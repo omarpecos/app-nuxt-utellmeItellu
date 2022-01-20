d5890daab39845f05801cc791fae94a81c4f463a
Omar Pecos <omarpecos@gmail.com>
2022-01-20 11:18:15 +0100

    Update: Remove project boilerplate

---
+++ pages/country.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row>
+    <v-col class="text-center"> Select here country/lang </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CountryPage',
+}
+</script>
