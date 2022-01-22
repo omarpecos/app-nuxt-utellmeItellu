9dd17de7b12506b0885056b72cd9f3891028782e
Omar Pecos <omarpecos@gmail.com>
2022-01-22 20:57:01 +0100

    New: CountryToolbar Update: Removed some elements from toolbar

---
+++ components/countries/countryToolbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <NuxtLink class="app-country-toolbar" v-if="hasConfig" to="/country">
+    <v-row justify="center" align="center">
+      <v-icon class="mr-2"> mdi-earth </v-icon>
+      <v-img
+        class="mr-2"
+        :max-height="18"
+        :max-width="24"
+        :src="selectedCountry.flag"
+        :alt="selectedCountry.name + '_flag'"
+      ></v-img>
+      <span>{{ selectedLang.name }}</span>
+    </v-row>
+  </NuxtLink>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Getter } from 'nuxt-property-decorator'
+import { Country } from '~/lib/models/country'
+import { Lang } from '~/lib/models/lang'
+
+@Component({
+  name: 'CountryToolbar',
+})
+export default class CountryToolbar extends Vue {
+  @Getter('selectedCountry') selectedCountry: Country
+  @Getter('selectedLang') selectedLang: Lang
+  @Getter('hasConfig') hasConfig: boolean
+}
+</script>
+
+<style lang="scss">
+.app-country-toolbar {
+  text-decoration: none !important;
+  width: fit-content;
+  padding: 1rem;
+  cursor: pointer;
+}
+</style>
